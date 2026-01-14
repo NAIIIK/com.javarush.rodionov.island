134d53e397844c7ed3256afa728b14c91b615733
RodionovNikita <radzivonaumikita@gmail.com>
2026-01-14 14:15:56 +0100

    fixes #2 14.01.2026

--- src/entity/carnivore/Carnivore.java
+++ src/entity/carnivore/Carnivore.java
@@ -3,4 +3,5 @@ package entity.carnivore;
 import entity.Animal;
 
 public class Carnivore extends Animal {
+    //TODO: поменять реализацию метода eat() (tryEat())
 }
--- src/entity/herbivore/Herbivore.java
+++ src/entity/herbivore/Herbivore.java
@@ -3,4 +3,5 @@ package entity.herbivore;
 import entity.Animal;
 
 public class Herbivore extends Animal {
+    //TODO: поменять реализацию метода eat() (tryEat())
 }
